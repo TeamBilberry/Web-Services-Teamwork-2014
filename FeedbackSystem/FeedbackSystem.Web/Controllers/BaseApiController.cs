fee4172b9ba516231566639d5589e474641a80dd
StefanSinapov <stefan.sinapov@gmail.com>
2014-09-18 02:19:56 +0300

    BaseApi Controller added

---
+++ FeedbackSystem/FeedbackSystem.Web/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+﻿namespace FeedbackSystem.Web.Controllers
+{
+    using System.Web.Http;
+    using Data.Contracts;
+
+//    [Authorize(Roles = "Admin")]
+    public abstract class BaseApiController : ApiController
+    {
+        protected IFeedbackSystemData Data;
+
+        protected BaseApiController(IFeedbackSystemData data)
+        {
+            this.Data = data;
+        }
+    }
+}
