38362a043f69ab6e2f65fbdef6dfa3ad2422d03d
StefanSinapov <stefan.sinapov@gmail.com>
2014-09-18 09:31:35 +0300

    error fix

--- FeedbackSystem/FeedbackSystem.Web/Controllers/ValuesController.cs
+++ FeedbackSystem/FeedbackSystem.Web/Controllers/ValuesController.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-
-namespace FeedbackSystem.Web.Controllers
+﻿namespace FeedbackSystem.Web.Controllers
 {
-    [Authorize]
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+    
+    //[Authorize]
     public class ValuesController : ApiController
     {
         // GET api/values
