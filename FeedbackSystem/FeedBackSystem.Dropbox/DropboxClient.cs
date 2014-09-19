87c7839d70dc953cddac7958a7824529571559c6
ADimanova <addi_91@abv.bg>
2014-09-19 10:49:55 +0300

    Dropbox File Storage added

---
+++ FeedbackSystem/FeedBackSystem.Dropbox/DropboxClient.cs
@@ -0,0 +1,14 @@
+﻿namespace DropboxUpload
+{
+    using System;
+    using System.Linq;
+    class DropboxClient
+    {
+        static void Main()
+        {
+            var droboxManager = new DropboxManager();
+            var imageName = droboxManager.LoadImage("../../smile-coffee.jpg");
+            Console.WriteLine("File {0} uploaded", imageName);
+        }
+    }
+}
---
+++ FeedbackSystem/FeedBackSystem.Dropbox/DropboxManager.cs
@@ -0,0 +1,78 @@
+﻿namespace DropboxUpload
+{
+    using System;
+    using System.IO;
+    using System.Diagnostics;
+
+    using Spring.Social.OAuth1;
+    using Spring.Social.Dropbox.Api;
+    using Spring.Social.Dropbox.Connect;
+    using Spring.IO;
+
+    class DropboxManager
+    {
+        private const string DropboxAppKey = "tpif8sxzq34mwcq";
+        private const string DropboxAppSecret = "680m327pz9558rh";
+
+        private const string OAuthTokenFileName = "OAuthTokenFileName.txt";
+
+        public string LoadImage(string path)
+        {
+            DropboxServiceProvider dropboxServiceProvider =
+                new DropboxServiceProvider(DropboxAppKey, DropboxAppSecret, AccessLevel.AppFolder);
+
+            // Authenticate the application (if not authenticated) and load the OAuth token
+            if (!File.Exists(OAuthTokenFileName))
+            {
+                AuthorizeAppOAuth(dropboxServiceProvider);
+            }
+            OAuthToken oauthAccessToken = LoadOAuthToken();
+
+            // Login in Dropbox
+            IDropbox dropbox = dropboxServiceProvider.GetApi(oauthAccessToken.Value, oauthAccessToken.Secret);
+
+            var place = "FeedbackSystemUploads";
+
+            // Upload a file
+            var extension = path.Split('.');
+            var imageName = string.Format("{0}.{1}", DateTime.Now.Ticks, extension[extension.Length - 1]);
+            Entry uploadFileEntry = dropbox.UploadFileAsync(
+                new FileResource(path),
+                string.Format("/{0}/{1}", place, imageName)).Result;
+            //Console.WriteLine("Uploaded a file: {0}", place);
+
+            // Share a file
+            DropboxLink sharedUrl = dropbox.GetShareableLinkAsync(uploadFileEntry.Path).Result;
+            Process.Start(sharedUrl.Url);
+
+            return imageName;
+        }
+
+        private static OAuthToken LoadOAuthToken()
+        {
+            string[] lines = File.ReadAllLines(OAuthTokenFileName);
+            OAuthToken oauthAccessToken = new OAuthToken(lines[0], lines[1]);
+            return oauthAccessToken;
+        }
+
+        private static void AuthorizeAppOAuth(DropboxServiceProvider dropboxServiceProvider)
+        {
+            // Authorization without callback url
+            OAuthToken oauthToken = dropboxServiceProvider.OAuthOperations.FetchRequestTokenAsync(null, null).Result;
+
+            OAuth1Parameters parameters = new OAuth1Parameters();
+            string authenticateUrl = dropboxServiceProvider.OAuthOperations.BuildAuthorizeUrl(
+                oauthToken.Value, parameters);
+            Process.Start(authenticateUrl);
+            Console.Write("Press [Enter] when authorization attempt has succeeded.");
+            Console.ReadLine();
+
+            AuthorizedRequestToken requestToken = new AuthorizedRequestToken(oauthToken, null);
+            OAuthToken oauthAccessToken =
+                dropboxServiceProvider.OAuthOperations.ExchangeForAccessTokenAsync(requestToken, null).Result;
+
+            string[] oauthData = new string[] { oauthAccessToken.Value, oauthAccessToken.Secret };
+            File.WriteAllLines(OAuthTokenFileName, oauthData);
+        }
+    }
+}
